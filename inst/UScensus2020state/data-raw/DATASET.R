33e1c56cb3cf46693bf282f243b807c38ccd8ae4
Neal Fultz <nfultz@ucla.edu>
2022-10-31 18:04:54 -0700

    States fixes

--- inst/UScensus2020state/data-raw/DATASET.R
+++ inst/UScensus2020state/data-raw/DATASET.R
@@ -1,2 +1,19 @@
 library(UScensus2020)
 UScensus2020:::build_package_data("state")
+
+
+# convert to single data frame
+
+states <- dir("data", ".*state20.rda", full.names = TRUE)
+
+e <- new.env()
+lapply(states, load, envir=e)
+states20 <- do.call(rbind, as.list(e))
+
+save(states20, file = "data/states20.rda")
+
+for(i in names(states20)){
+  comment(states20[[i]]) <- comment(e$akstate20[[i]])
+}
+
+unlink(states)
